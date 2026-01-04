2d4b75bdbbc9ed84c81e444d2b2b7afb18aa456e
yadokani389 <yadokani389@gmail.com>
2026-01-04 14:23:01 +0900

    tiny-code-action: update

--- config/tiny-code-action.nix
+++ config/tiny-code-action.nix
@@ -8,8 +8,8 @@
         src = pkgs.fetchFromGitHub {
           owner = "rachartier";
           repo = "tiny-code-action.nvim";
-          rev = "597c4a39d5601e050d740f3ef437ee695d1ff3b0";
-          hash = "sha256-+U1GUvfLPZ+4MPi7Q5LG8TJEWJHyS45qbg1dpBk7g98=";
+          rev = "897de9af9e3aa6e59bcb9fec760a7eb02d00e264";
+          hash = "sha256-tiV+drfWAryw8cexSCgmZCXfHxi4oi6qX6oNmhHrhAk=";
         };
         dependencies = with pkgs.vimPlugins; [
           telescope-nvim
